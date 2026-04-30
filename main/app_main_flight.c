0e74c7c6429946313e24fc3abd16b87723c78d00
peakxie <peakxie@tencent.com>
2026-04-30 09:55:40 +0800

    feat: auto config

--- main/app_main_flight.c
+++ main/app_main_flight.c
@@ -17,27 +17,27 @@
 void appMain(void) {
   DEBUG_PRINT("APP started!\n");
 
-  /* 等待 30 秒: 陀螺仪校准 + 传感器稳定 */
-  DEBUG_PRINT("Waiting 30s...\n");
-  vTaskDelay(M2T(30000));
+  /* 等待 10 秒: 陀螺仪校准 + 传感器稳定 */
+  DEBUG_PRINT("Waiting 10s...\n");
+  vTaskDelay(M2T(10000));
 
   /* 启用 high-level commander */
   paramVarId_t idHL = paramGetVarId("commander", "enHighLevel");
   paramSetInt(idHL, 1);
   vTaskDelay(M2T(500));
 
-  /* 起飞到 0.2m, 1 秒上升 */
-  DEBUG_PRINT("Takeoff 0.2m...\n");
-  crtpCommanderHighLevelTakeoff(0.2f, 1.0f);
+  /* 起飞到 0.5m, 5 秒上升 */
+  DEBUG_PRINT("Takeoff 0.5m...\n");
+  crtpCommanderHighLevelTakeoff(0.5f, 5.0f);
   vTaskDelay(M2T(1500));
 
   /* 悬停 2 秒 */
-  DEBUG_PRINT("Hover 2s...\n");
-  vTaskDelay(M2T(2000));
+  // DEBUG_PRINT("Hover 2s...\n");
+  // vTaskDelay(M2T(2000));
 
-  /* 降落, 1.5 秒下降 */
+  /* 降落, 3 秒下降 */
   DEBUG_PRINT("Land...\n");
-  crtpCommanderHighLevelLand(0.0f, 1.5f);
+  crtpCommanderHighLevelLand(0.0f, 3.0f);
   vTaskDelay(M2T(2000));
 
   DEBUG_PRINT("Done.\n");
